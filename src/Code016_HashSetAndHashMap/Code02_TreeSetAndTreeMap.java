9e22c5133f69b10b7720c594904a1cbb5648736d
T-zhengke <zhengke996@126.com>
2025-01-19 10:53:29 +0800

    feat(learn): 添加 TreeSet 和 TreeMap 的示例代码
    - 新增 Code02_TreeSetAndTreeMap.java 文件
    - 实现了 TreeMap 的基本操作，包括插入、查询、更新和删除
    - 展示了 TreeMap 的一些特性，如自动排序和获取最小/最大键值
    - 添加了 TreeSet 的示例，演示去重和排序功能
    - 包含 PriorityQueue 的示例，展示了小根堆和大根堆的使用

---
+++ src/Code016_HashSetAndHashMap/Code02_TreeSetAndTreeMap.java
@@ -0,0 +1,73 @@
+package Code016_HashSetAndHashMap;
+
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Code02_TreeSetAndTreeMap {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(5, "这是5");
+        treeMap.put(7, "这是7");
+        treeMap.put(1, "这是1");
+        treeMap.put(2, "这是2");
+        treeMap.put(3, "这是3");
+        treeMap.put(4, "这是4");
+        treeMap.put(8, "这是8");
+
+        System.out.println(treeMap.containsKey(1));// true
+        System.out.println(treeMap.containsKey(10));// false
+        System.out.println(treeMap.get(4));// 这是4
+        treeMap.put(4, "张三是4");
+        System.out.println(treeMap.get(4));// 张三是4
+
+        treeMap.remove(4);
+        System.out.println(treeMap.get(4) == null);// true
+
+        System.out.println(treeMap.firstKey());// 1
+        System.out.println(treeMap.lastKey());// 8
+
+        // TreeMap中，所有的key，<= 4且最近的key是什么
+        System.out.println(treeMap.floorKey(4));// 3
+
+        // TreeMap中，所有的key，>= 4且最近的key是什么
+        System.out.println(treeMap.ceilingKey(4));// 5
+
+        System.out.println("---------------------------------------------------");
+        TreeSet<Integer> set = new TreeSet<>(); // set weight removal
+        set.add(3);
+        set.add(3);
+        set.add(4);
+        set.add(4);
+        System.out.println("有序表大小 : " + set.size()); // 2 去重
+        while (!set.isEmpty()) {
+            System.out.println(set.pollFirst());// 3 4
+//            System.out.println(set.pollLast());// 4 3
+        }
+        System.out.println("---------------------------------------------------");
+        // 堆，默认小根堆、如果要大根堆，定制比较器！
+        PriorityQueue<Integer> heap1 = new PriorityQueue<>();
+        heap1.add(3);
+        heap1.add(3);
+        heap1.add(4);
+        heap1.add(4);
+        System.out.println("堆大小 : " + heap1.size()); // 4
+        while (!heap1.isEmpty()) {
+            System.out.println(heap1.poll());
+        }
+        System.out.println("---------------------------------------------------");
+        // 定制的大根堆，用比较器！
+        PriorityQueue<Integer> heap2 = new PriorityQueue<>((a, b) -> b - a);
+        heap2.add(3);
+        heap2.add(3);
+        heap2.add(4);
+        heap2.add(4);
+        System.out.println("堆大小 : " + heap2.size());
+        while (!heap2.isEmpty()) {
+            System.out.println(heap2.poll());
+        }
+        System.out.println("---------------------------------------------------");
+
+
+    }
+}
